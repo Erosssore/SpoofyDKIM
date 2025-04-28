9a971f62b36b2e27a5a1ccb028ff734370b98e03
soldudo <88810277+soldudo@users.noreply.github.com>
2025-04-28 14:11:05 -0500

    added dkim module

---
+++ modules/dkim.py
@@ -0,0 +1,27 @@
+import dns.resolver
+
+USUAL_SELECTORS = ["default", "google", "selector1", "mail", "spf", "dkim"]
+
+class DKIM:
+    def __init__(self, domain, dns_server=None):
+        self.domain = domain
+        self.dns_server = dns_server
+        self.dkim_record = self.get_dkim_record()
+        self.version = None
+        self.algorithm = None
+        self.selector = None
+        self.public_key = None
+
+
+        if self.dkim_record:
+            self.version = self.get_dkim_version()
+            self.algorithm = self.get_dkim_algorithm()
+            self.public_key = self.get_dkim_public_key()
+
+    def get_dkim_record(self):
+        """Returns the DKIM record for the domain."""
+        try:
+            resolver = dns.resolver.Resolver()
+            if self.dns_server:
+                resolver.nameservers = [self.dns_server]
+            
