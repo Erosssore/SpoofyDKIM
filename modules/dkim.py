a27b4504e8b97720b344b8a50e433cb0a94e9a82
soldudo <88810277+soldudo@users.noreply.github.com>
2025-04-28 21:07:11 -0500

    dkim object now reports algorithm and key

--- modules/dkim.py
+++ modules/dkim.py
@@ -9,7 +9,6 @@ class DKIM:
         self.dkim_record = self.get_dkim_record()
         self.version = None
         self.algorithm = None
-        self.selector = None
         self.public_key = None
 
 
@@ -33,8 +32,9 @@ class DKIM:
     def find_dkim_selector(self):
         """Finds the DKIM selector for the domain."""
         resolver = dns.resolver.Resolver()
-        if self.dns_server:
-                resolver.nameservers = [self.dns_server]
+        #using dns_server leads to unreliable dkim acquisition
+        #if self.dns_server:
+         #       resolver.nameservers = [self.dns_server]
 
         for selector in USUAL_SELECTORS:
             query = f"{selector}._domainkey.{self.domain}"
@@ -45,3 +45,20 @@ class DKIM:
             txts = [b"".join(rdata.strings).decode("utf-8") for rdata in answers]
             return selector, txts
                 
+    def get_dkim_version(self):
+        """Returns the DKIM version from the DKIM record."""
+        if "v=" in self.dkim_record:
+            return self.dkim_record.split("v=")[1].split(";")[0]
+        return None
+    
+    def get_dkim_algorithm(self):
+        """Returns the DKIM algorithm from the DKIM record."""
+        if "k=" in self.dkim_record:
+            return self.dkim_record.split("k=")[1].split(";")[0]
+        return None
+    
+    def get_dkim_public_key(self):
+        """Returns the DKIM public key from the DKIM record."""
+        if "p=" in self.dkim_record:
+            return self.dkim_record.split("p=")[1].split(";")[0]
+        return None
